125f1227ca6987c4436c1f1904653e4255dde72a
Your Name <you@example.com>
2026-05-18 03:07:26 +0000

    new try

--- graph.c
+++ graph.c
@@ -62,7 +62,7 @@ void addEdge(Graph* g, const char* src, const char* dest, int weight) {
 
 List* getEdges(Graph* g, const char* label) {
     if (!g || !label) return NULL;
-    MApPair* p = map_search(g->adjacencyMap, (void*)label);
+    MapPair* p = map_search(g->adjacencyMap, (void*)label);
     if(!p) return NULL;
     return (List*)p->value;
 }
